2849881d70d691c802bacf284a425894475b622d
Probook-hp <nameyepwhite@gmail.com>
2024-12-09 22:45:34 +0300

    Update program.py from github

--- program.py
+++ program.py
@@ -3,4 +3,4 @@ print('Hello world!')
 for i in range(1):
     print('git is good!')
 
-my_info = "Эту строчку я добавил с помощью интерфейса github"
+my_info = "Переписал строчку с помошью гитхаба."
